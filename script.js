56bc66bbf8f5fd2c9aecec593fc65820212c3594
miura <miura@example.com>
2026-05-01 12:12:09 +0900

    おみくじの結果増やしたよ

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const fortunes = [
-    "大吉!運いいな！",
+    "超大吉だ！！ほんまえぐいっすわ！",
+    "大吉！！運いいね",
     "中吉🤗( ^^) _U~~",
     "小吉😇今日はゆっくりすごそう",
-    "凶😭次がんばれよ"
+    "凶😭次がんばれよ",
+    "小凶　小凶ハンパないって"
 ];
 
 const button = document.getElementById("a");
@@ -10,5 +12,7 @@ const result = document.getElementById("b");
 
 button.addEventListener("click", function(){
     const randomNumber = Math.floor(Math.random()*fortunes.length);
+    if (result="超大吉だ！！ほんまえぐいっすわ！"){result.style.color ="yellow"};
     result.textContent = fortunes[(randomNumber)];  
 });
+
